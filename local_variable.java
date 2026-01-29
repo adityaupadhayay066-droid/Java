a285a0139ba3d91c5c4405209f3515493a76a9be
Aditya Upadhayay <adityaupadhayay066@gmail.com>
2026-01-29 14:54:16 +0530

    Instance variable in Java

---
+++ local_variable.java
@@ -0,0 +1,13 @@
+// local variable can be create inside a block of class 
+
+public class local_variable {
+    void num(){
+        int a =18;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+         local_variable maths = new local_variable();
+        maths.num();
+    }
+}
+ 
---
+++ swap.java
@@ -0,0 +1,10 @@
+public class swap {
+    public static void main(String[] args){
+        int a =10;
+        int b = 20;
+        System.out.print("Before swapping " );
+        swap(a,b);
+        System.out.print("After swapping");
+    }
+    
+}
