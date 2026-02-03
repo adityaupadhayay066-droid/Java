ead31a4dc91c829708cd19dd60e156f3d63c0c9b
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-03 09:45:02 +0530

    Variables in Java

---
+++ Variable.java
@@ -0,0 +1,10 @@
+class Variable{
+    public static void main(String[] args) {
+        int a = 18, b=54;
+        int sum = a+b;
+        System.out.println(sum);
+        // System.out.printf("Sum of variables are : %d", sum);
+        int sub = b-a ;
+        System.out.println(sub);
+    }
+}
