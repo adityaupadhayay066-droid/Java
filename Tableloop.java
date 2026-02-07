88bb8d927b7e3458e47e47c002d66eef8d6222a6
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-07 09:50:09 +0530

    Add multiplication table program using while loop

---
+++ Tableloop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Tableloop {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter your number here : ");
+        int n = sc.nextInt();
+        // for(int i=1; i<=10; i++){
+        //     System.out.println(n+"x"+i+"=" +(i*n));
+        int i =1;
+        while(i<=10){
+            System.out.println(n+"x"+i +"=" +(i*n));
+            i++;
+
+        }
+
+        }
+        
+    }
+
+    
