2a488b9e27e5985745dd69f834d059ada774c906
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-17 22:09:32 +0530

    Added OOPS folder with file

---
+++ OOPS/GetterSetter.java
@@ -0,0 +1,46 @@
+class Employee{
+   public  String name;
+    private int  EmpId;
+    private double salary;
+    String Dept;
+
+    public void setEmpId(int e){
+        EmpId = e;
+    }
+    public int getEmpId(){
+        return EmpId;
+    }
+    public void setname(String n){
+        name = n;
+    }
+    public String getname(){
+        return name;
+    }
+public void setsalary(double s){
+    salary = s;
+}
+public double getsalary(){
+    return salary;
+}
+// public void setDept(String d){
+//     Dept = d;
+// }
+// public String getDept(){
+//     return Dept;
+// }
+}
+
+public class GetterSetter {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.setEmpId(101);
+        emp.setname("Aditya Upadhayay");
+        emp.setsalary(800000);
+        emp.Dept("Ai and ML ");
+        System.out.println("Employee id is : "+emp.getEmpId());
+         System.out.println("Employee name is  : "+emp.getname());
+          System.out.println("Employee salary is : "+emp.getsalary());
+           System.out.println("Employee Department is  : "+emp.Dept());
+    }
+    
+}
