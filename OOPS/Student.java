b6149aebbc4c842e71b8f28437bc7f8bcd2b4561
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-18 08:53:33 +0530

    Create Student class with Detail object and print data

---
+++ OOPS/Student.java
@@ -0,0 +1,18 @@
+class Detail {
+    String name = "Aditya Upadhayay";
+    int RollNo = 46;
+    String Department = "BCA";
+    int age = 18;
+
+
+}
+
+public class Student {
+    public static void main(String[] args) {
+        Detail d = new Detail();
+        System.out.println(d.name);
+        System.out.println(d.RollNo);
+        System.out.println(d.Department);
+         System.out.println(d.age);
+    }
+}
