1cbd553167fe5131d98c614574983dcc4a8f93c3
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-19 11:00:01 +0530

    Static variable program in Java

---
+++ OOPS/polymorphism.java
@@ -0,0 +1,21 @@
+class Student{
+    void marks(int a){
+        System.out.println("you failed in C++");
+    }
+    void marks(int  a, int b){
+        System.out.println("You failed in DBMS");
+    }
+    void marks(int a, int b , int c){
+        System.out.println("You failed in life");
+    }
+
+}
+public class polymorphism{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.marks(20);
+        s.marks(10,11);
+        s.marks(20,60,10);
+    }
+
+}
---
+++ static.java
@@ -0,0 +1,9 @@
+class Detail{
+    static String collegeName;
+}
+public class vtype{
+    public static void main(String[] args) {
+        Detail.collegeName = "CUTM";
+        System.out.println("College name is : "+ Detail.collegeName);
+    }
+}
