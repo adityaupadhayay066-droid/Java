a92ae511be3a7f291a127128ce920bc1bce30166
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-22 12:29:52 +0530

    MultiLevel inheritance programm in Java

---
+++ OOPS/Multilevel.java
@@ -0,0 +1,28 @@
+// Multilevel inheritacne has one base 
+
+class Human {  // parent class
+    void human(){
+    System.out.println("Human still alive");
+}
+
+}
+class Eat extends Human{ 
+    void eat(){
+        System.out.println("Human are eating");
+    }
+
+}
+class Working extends Eat{  // Derived class form Eat 
+    void work(){
+        System.out.println("Human are Working");
+    }
+
+}
+public class Multilevel extends Working{
+    public static void main(String[] args) {
+        Multilevel M = new Multilevel();
+        M.human();
+        M.eat();
+        M.work();
+    }
+}
