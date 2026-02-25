fdce2d73cde7765aa7c7e49b97454557ea9ad05b
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-25 20:42:01 +0530

    Find the length of String in Java program

---
+++ String_Example.java
@@ -0,0 +1,7 @@
+class String_Example {
+    public static void main(String[] args) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.print("The Length of string is : "+alphabet.length());
+    }
+    
+}
